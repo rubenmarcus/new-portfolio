dd5318e5c312f107a648b15bcfde51660eba8141
Ruben Marcus <ruben@rubenmarcus.com>
2021-03-28 14:03:12 -0300

    Main

--- src/main.js
+++ src/main.js
@@ -5,12 +5,8 @@ export default function (Vue, { head }) {
   if (process.isClient) {
     const VueTyper = require('vue-typer').default;
     Vue.use(VueTyper);
-    console.log(navigator.languages, 'language')
     const isPt = checkLanguage()
-    console.log(checkLanguage(), isPt)
-
-
-    Vue.prototype.$lang = 'en'
+      Vue.prototype.$lang = 'en'
 
     if(isPt){
       head.htmlAttrs = { lang: "pt-BR"}
